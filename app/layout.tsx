097b33b7f9864ce8e0aa48dfedbe5014770ac82b
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-11 21:09:49 +0100

    Setup project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Onest } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const onest = Onest({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={onest.className}>{children}</body>
     </html>
   )
 }
