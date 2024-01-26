bf9db83d74f799f05ad44de24bc84319c8e66efe
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-26 10:39:47 +0100

    Fix scroll bug

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center gap-6 black w-full">
+      <main className="flex flex-col items-center gap-6 black w-full">
         <ThemeSwitch />
         <Form />
         <UrlAlert />
